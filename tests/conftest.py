029f538193759c83b919f2336031f09f3dead881
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-06-23 16:38:33 +0300

    подготовка проекта

---
+++ tests/conftest.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime
+
+import pytest
+from dotenv import load_dotenv
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_config(request):
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    browser.config.window_height = 2500
+    browser.config.window_width = 1400
+    browser.config.type_by_js = True
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+
+    browser.quit()
